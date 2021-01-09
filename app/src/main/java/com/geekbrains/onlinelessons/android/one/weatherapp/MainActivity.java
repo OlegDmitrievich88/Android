a4f80fd50d0f6c759975b2f2e115775b1ddf8fbd
OlegDmitrievich88 <70801999+OlegDmitrievich88@users.noreply.github.com>
2021-01-09 12:35:12 +0300

    First DZ commit

--- app/src/main/java/com/geekbrains/onlinelessons/android/one/weatherapp/MainActivity.java
+++ app/src/main/java/com/geekbrains/onlinelessons/android/one/weatherapp/MainActivity.java
@@ -4,23 +4,74 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
       private Button button;
+      private static final String tag = "activity";
+      private static ImageView imageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        Toast.makeText(this, "hello man",Toast.LENGTH_SHORT).show();
+        Log.d(tag,"Create");
         initViews();
         Button();
+        //setImageView();
 
     }
 
+    public void setImageView() {
+        imageView.setImageDrawable(getDrawable(R.drawable.winter));
+    }
 
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Toast.makeText(this, "start",Toast.LENGTH_SHORT).show();
+        Log.d(tag,"Start");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Toast.makeText(this, "resume",Toast.LENGTH_SHORT).show();
+        Log.d(tag,"Resume");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Toast.makeText(this, "pause",Toast.LENGTH_SHORT).show();
+        Log.d(tag,"Pause");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Toast.makeText(this, "stop",Toast.LENGTH_SHORT).show();
+        Log.d(tag,"Stop");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Toast.makeText(this, "restart",Toast.LENGTH_SHORT).show();
+        Log.d(tag,"Restart");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Toast.makeText(this, "destroy",Toast.LENGTH_SHORT).show();
+        Log.d(tag,"Destroy");
+    }
 
     private void Button(){
         button.setOnClickListener(new View.OnClickListener() {
@@ -34,10 +85,8 @@ public class MainActivity extends AppCompatActivity {
 
     private void initViews() {
           button = findViewById(R.id.button1);
+          imageView = findViewById(R.id.imageView);
     }//инициализирует
 
-//    public void createNewWindow(View view) {
-//        Intent intent = new Intent(this, ScreenCity.class);
-//        startActivity(intent);
-//    }
+
 }
--- app/src/main/java/com/geekbrains/onlinelessons/android/one/weatherapp/SettingForCity.java
+++ app/src/main/java/com/geekbrains/onlinelessons/android/one/weatherapp/SettingForCity.java
@@ -1,12 +1,16 @@
 package com.geekbrains.onlinelessons.android.one.weatherapp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.PersistableBundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.ImageView;
 import android.widget.RadioButton;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class SettingForCity extends AppCompatActivity {
@@ -14,35 +18,90 @@ public class SettingForCity extends AppCompatActivity {
     private RadioButton radioButton2;
     private ImageView imageView;
     private Button accept;
-    private CheckBox checkBox;
-
+    private CheckBox checkBox1;
+    private CheckBox checkBox2;
+    private CheckBox checkBox3;
+    private CheckBox checkBox4;
+    private boolean isChecked1;
+    private boolean isChecked2;
+    private boolean isChecked3;
+    private boolean isChecked4;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.setting_window);
         initViews();
+        checkBoxListener();
+       // Intent set = new Intent(SettingForCity.this,MainActivity.class);
+
+       // setRadioButton();
     }
-    private void setRadioButton() {
-        radioButton3.setOnClickListener(new View.OnClickListener() {
+
+    private void checkBoxListener() {
+       checkBox1.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+           @Override
+           public void onCheckedChanged(CompoundButton compoundButton, boolean isChecked) {
+               if (checkBox1.isChecked()){
+                   isChecked1 = true;
+               }
+           }
+       });
+        checkBox2.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
-            public void onClick(View view) {
-                imageView.setImageDrawable(getDrawable(R.drawable.sun));
+            public void onCheckedChanged(CompoundButton compoundButton, boolean isChecked) {
+                if (checkBox2.isChecked()){
+                    isChecked2 = true;
+                }
             }
         });
-        radioButton2.setOnClickListener(new View.OnClickListener() {
+        checkBox3.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
-            public void onClick(View view) {
-                imageView.setImageDrawable(getDrawable(R.drawable.winter));
+            public void onCheckedChanged(CompoundButton compoundButton, boolean isChecked) {
+                if (checkBox3.isChecked()){
+                    isChecked3 = true;
+                }
             }
         });
-    }// меняет темы на главном экране
+        checkBox4.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean isChecked) {
+              if (checkBox4.isChecked()){
+                  isChecked4 = true;
+              }
+
+
+            }
+        });
+    }
+
+
     private void initViews() {
           radioButton3 = findViewById(R.id.radioButton3);
           radioButton2 = findViewById(R.id.radioButton2);
           imageView = findViewById(R.id.imageView);
-      //    accept = findViewById(R.id.acceptSetting);
-       //   checkBox = findViewById(R.id.temp);
-      //    checkBox = findViewById(R.id.pressure1);
+          accept = findViewById(R.id.acceptSetting);
+         checkBox1 = findViewById(R.id.temp);
+          checkBox2 = findViewById(R.id.pressure1);
+          checkBox3 = findViewById(R.id.wind1);
+          checkBox4 = findViewById(R.id.wet1);
     }//инициализирует
+
+    @Override
+    public void onSaveInstanceState(@NonNull Bundle outState) {
+        outState.getBoolean(String.valueOf(checkBox1),isChecked1);
+        outState.getBoolean(String.valueOf(checkBox2),isChecked2);
+        outState.getBoolean(String.valueOf(checkBox3),isChecked3);
+        outState.getBoolean(String.valueOf(checkBox4),isChecked4);
+        super.onSaveInstanceState(outState);
+    }
+
+    @Override
+    protected void onRestoreInstanceState(@NonNull Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        checkBox1.setChecked(savedInstanceState.getBoolean(String.valueOf(checkBox1),isChecked1));
+        checkBox2.setChecked(savedInstanceState.getBoolean(String.valueOf(checkBox2),isChecked2));
+        checkBox3.setChecked(savedInstanceState.getBoolean(String.valueOf(checkBox3),isChecked3));
+        checkBox4.setChecked(savedInstanceState.getBoolean(String.valueOf(checkBox4),isChecked4));
+    }
 }
 
